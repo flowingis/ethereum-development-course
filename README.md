d0506f340a7fd482f8f99c24a8ee5c8ec2c2c4a5
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-09-10 17:56:43 +0200

    added standardJS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ethereum-development-course
 Material for a Ethereum Development Course
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
--- test/BasicCounter.js
+++ test/BasicCounter.js
@@ -8,12 +8,12 @@ contract('BasicCounter', () => {
     contract = await BasicCounter.new()
   })
 
-  it("counter should start with 0", async () => {
+  it('counter should start with 0', async () => {
     const value = await contract.counter()
     assert.equal(value.valueOf(), 0)
   })
 
-  it("increment should increment counter", async () => {
+  it('increment should increment counter', async () => {
     await contract.increment()
     const value = await contract.counter()
     assert.equal(value.valueOf(), 1)
