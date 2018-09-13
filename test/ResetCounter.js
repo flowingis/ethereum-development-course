44b20aebd86c3333ef5324273e986a0df1df01b8
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-09-13 15:16:12 +0200

    cleanup

--- test/ResetCounter.js
+++ test/ResetCounter.js
@@ -8,17 +8,6 @@ contract('ResetCounter', () => {
     contract = await ResetCounter.new()
   })
 
-  it('counter should start with 0', async () => {
-    const value = await contract.counter()
-    assert.equal(value.valueOf(), 0)
-  })
-
-  it('increment should increment counter', async () => {
-    await contract.increment()
-    const value = await contract.counter()
-    assert.equal(value.valueOf(), 1)
-  })
-
   it('reset should reset counter', async () => {
     await contract.increment()
     await contract.reset()
