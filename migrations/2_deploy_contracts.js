46ea2c680eda027f3ca8396af571d9f4ca0054c3
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-09-14 21:17:27 +0200

    added tutorial token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const ConvertLib = artifacts.require('./ConvertLib.sol')
 const BasicToken = artifacts.require('./BasicToken.sol')
+const TutorialToken = artifacts.require('./TutorialToken.sol')
 
 module.exports = deployer => {
   deployer.deploy(ConvertLib)
   deployer.link(ConvertLib, BasicToken)
   deployer.deploy(BasicToken)
+  deployer.deploy(TutorialToken)
 }
