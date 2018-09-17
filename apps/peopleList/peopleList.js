3f2e7042b763491ba63c550beaeec45b6621d31b
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-09-17 15:56:32 +0200

    completed people list

--- apps/peopleList/peopleList.js
+++ apps/peopleList/peopleList.js
@@ -1,6 +1,6 @@
 import swal from 'sweetalert2'
 import template from './peopleList.html'
-import { initWeb3 } from 'apps/utils'
+import { initWeb3, htmlToElement } from 'apps/utils'
 import contractFactory from './contract'
 
 const web3 = initWeb3()
@@ -11,6 +11,9 @@ class PeopleListApp extends HTMLElement {
     this.contract = contractFactory(web3)
     this.getAccount()
     this.attachEventListeners()
+    this.inputName = this.querySelector('[data-person-name]')
+    this.inputBirthdate = this.querySelector('[data-person-birthdate]')
+    this.inputSex = this.querySelector('[data-person-sex]')
   }
 
   async getAccount () {
@@ -25,12 +28,17 @@ class PeopleListApp extends HTMLElement {
     this.querySelector('[data-connect]').addEventListener('click', () => {
       this.connect(this.querySelector('[data-input-contract]').value)
     })
+
+    this.querySelector('[data-add]').addEventListener('click', () => {
+      this.add()
+    })
   }
 
   async deploy (owner) {
     try {
       await this.contract.deploy(owner)
       this.querySelector('[data-contract-address]').innerText = this.contract.address
+      await this.list()
     } catch (e) {
       swal({
         title: 'Error!',
@@ -44,6 +52,7 @@ class PeopleListApp extends HTMLElement {
     try {
       await this.contract.connect(address)
       this.querySelector('[data-contract-address]').innerText = this.contract.address
+      await this.list()
     } catch (e) {
       swal({
         title: 'Error!',
@@ -51,6 +60,33 @@ class PeopleListApp extends HTMLElement {
         type: 'error'
       })
     }
+  }
+
+  async add () {
+    const person = {
+      name: this.inputName.value,
+      birthdate: new Date(this.inputBirthdate.value).getTime(),
+      sex: this.inputSex.value
+    }
+
+    await this.contract.add({
+      person,
+      address: this.account
+    })
+
+    await this.list()
+  }
+
+  async list () {
+    const users = await this.contract.list()
+
+    const list = this.querySelector('[data-events-list]')
+    list.innerHTML = ''
+    users
+      .map(user => htmlToElement(`<div class="item">${user.name}</div>`))
+      .forEach(element => {
+        list.appendChild(element)
+      })
   }
 }
 
